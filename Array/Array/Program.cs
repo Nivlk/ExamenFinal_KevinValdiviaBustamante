db573ab9e88967bef99f152ccd849aa07f83cc00
Nivlk <Sistemkev@gmail.com>
2021-10-29 09:32:36 -0400

    Ejercicio Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t, n;
+            int[] a;
+            Console.WriteLine("Ingrese el tamaño del Array: ");
+            t = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero para sacar los multiplos: ");
+            n = int.Parse(Console.ReadLine());
+            a = new int[t];
+            for (int f = 0; f < t; f++)
+            {
+                a[f] = (f + 1) * n;
+                Console.WriteLine("Los multiplos son: " + a[f]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 # Contenedor ubuntu kevin v.0.1
 FROM ubuntu:18.04
 RUN apt-get update -y
-RUN apt-get install apache2 -y && apt-get install nginx -y && apt-get install Terminator -y
+RUN apt-get install apache2 -y 
+RUN apt-get install nginx -y 
+RUN apt-get install terminator -y
 ENTRYPOINT echo "Todo Posi..."
